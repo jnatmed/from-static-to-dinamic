1edbc16d235ef54afabc49dca6ec6959c1163075
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-04-10 17:21:44 -0300

    se agrega logs

--- public/index.php
+++ public/index.php
@@ -4,8 +4,15 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Paw\App\Controllers\PageController;
 use Paw\App\Controllers\ErrorController;
+use Monolog\Logger;
+use Monolog\Level;
+use Monolog\Handler\StreamHandler;
+
 
 $whoops = new \Whoops\Run;
+
+$log = new Logger('mvc-app');
+$log->pushHandler(new StreamHandler(__DIR__.'/../logs/app.log', Level::Debug));
 
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 
@@ -13,19 +20,26 @@ $whoops->register();
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+$log->info("Peticion a :{$path}");
+
 $controller = new PageController();
 
 if ($path === '/') {
     $controller->index();
+    $log->info("Respuesta Exitosa: 200");
 } elseif ($path == '/about') {
     $controller->about();    
+    $log->info("Respuesta Exitosa: 200");
 } elseif ($path == '/contact') {
     $controller->contact();
+    $log->info("Respuesta Exitosa: 200");
 } elseif ($path == '/services') {
     $controller->services();    
+    $log->info("Respuesta Exitosa: 200");
 } else {
     $controller = new ErrorController();
     $controller->notFound();
+    $log->info("Path Not Found: 404");
 }
     
 
